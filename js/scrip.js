7902340b60023b764d91c76e0fc7a7f57edc08e3
YADER503 <yaderomerot@gmail.com>
2025-09-23 08:25:29 -0600

    first commit

---
+++ js/scrip.js
@@ -0,0 +1,40 @@
+const txtN1 = document.getElementById("n1");
+const txtOperador = document.getElementById("Operador");
+const txtN2 = document.getElementById("n2");
+const btnCalcular = document.getElementById("Calcular");
+const pResultado = document.getElementById("Resultado");
+
+btnCalcular.addEventListener("click", Calcular );
+
+function Calcular(){ 
+    const n1 = parseFloat(txtN1.value)
+    const n2 = parseFloat(txtN2.value)
+    const operador = txtOperador.value; 
+
+    if ( (operador == "+" || operador == "-" || operador == "*" || operador == "/" ) && !isNaN(n1) && !isNaN(n2) ) {
+        let resultado;
+        switch (operador) {
+            case "+":
+             resultado =   n1 + n2
+                break;
+            case "-":
+             resultado =   n1 - n2
+                break;
+            case "*":
+             resultado =   n1 * n2
+                break;
+            case "/":
+             resultado =   n1 / n2
+                break;
+        }
+        pResultado.innerText = "=" + resultado
+        pResultado.style = "Color: blue";
+    } else {
+        pResultado.innerText = "Resultado imposible"
+        pResultado.style.color = "red"
+
+
+    }
+
+
+}
